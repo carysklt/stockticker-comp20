378a765ff546b058a3df8fb086bdabce56b11ced
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-10 01:50:29 +0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var url = require('url');
+const MongoClient = require('mongodb').MongoClient;
+const link = "mongodb+srv://carysklt:carysklt123@cluster0.nipyz.mongodb.net/?retryWrites=true&w=majority";
+
+console.log("hello world");
+http.createServer(function (req, res) {
+  MongoClient.connect(link, function(err, db) {
+    if(err) { throw err; }
+  
+    //take querystring from html form and parse it
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var whichOne = q.choice;
+    var what = q.querystring;
+    
+    //indicate database and collection
+    var dbo = db.db("StockTicker");
+    var collection = dbo.collection('companies');
+    if(q.choice === "companyName"){
+      var result = collection.find({"Company": q.querystring});
+      console.log(result);
+    } else {
+      collection.find({"Ticker": q.querystring});
+    }
+  // var qobj = url.parse(req.url, true).query;
+  // var txt = qobj.name;   // assume x is querystring parameter
+  
+  //check whether it is company or ticker
+  //take the info and search for it
+  
+    // res.end("The value is: " + what);
+
+    db.close();
+  });
+}).listen(8080);
+
+
+// theQuery = {author:"Bob Smith"};
+// coll.find(theQuery).toArray(function(err, items) 
+// {if (err) {console.log("Error: " + err);} 
+// else 
+// {
+//   console.log("Items: ");
+// for (i=0; i<items.length; i++)
+// console.log(i + ": " + items[i].title + " by: " + items[i].author);
+// }  
+//  db.close();
