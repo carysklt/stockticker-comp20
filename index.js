8aa1a36eb81d5efc1b77d339eda9cb9be59bf249
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-11 23:16:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const MongoClient = require('mongodb').MongoClient;
 const link = "mongodb+srv://carysklt:carysklt123@cluster0.nipyz.mongodb.net/?retryWrites=true&w=majority";
 
 http.createServer(function (req, res) {
-  MongoClient.connect(link, function(err,db){
-  //indicate database and collection
-     var dbo = db.db('StockTicker');
-     var collection = dbo.collection('companies');
-//   });
+
   res.writeHead(200, {'Content-Type': 'text/html'});
   //take query string from html form and parse it
   
   var q = url.parse(req.url, true).query;
   var whichOne = q.choice;
   var what = q.querystring;
+  
+   MongoClient.connect(link, function(err,db){
+  //indicate database and collection
+     var dbo = db.db('StockTicker');
+     var collection = dbo.collection('companies');
   });
+//   });
    if(q.choice === "companyName"){
 //      collection.findOne({"Company": q.querystring},function(err,result){
        res.end("The stock ticker for " + q.querystring + " is ");
