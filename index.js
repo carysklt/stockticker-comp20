1315301d4212ca475207c0659b563229f1ccbeea
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-11 14:36:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 var http = require('http');
 var url = require('url');
-const fs = require('fs');
-const MongoClient = require('mongodb').MongoClient;
-const link = "mongodb+srv://carysklt:carysklt123@cluster0.nipyz.mongodb.net/?retryWrites=true&w=majority";
+// const fs = require('fs');
+// const MongoClient = require('mongodb').MongoClient;
+// const link = "mongodb+srv://carysklt:carysklt123@cluster0.nipyz.mongodb.net/?retryWrites=true&w=majority";
 
 http.createServer(function (req, res) {
   console.log("hello world");
-  MongoClient.connect(link, function(err, db) {
-    if(err) { throw err; }
+//   MongoClient.connect(link, function(err, db) {
+//     if(err) { throw err; }
   
-    //take querystring from html form and parse it
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var whichOne = q.choice;
-    var what = q.querystring;
+//     //take querystring from html form and parse it
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var q = url.parse(req.url, true).query;
+//     var whichOne = q.choice;
+//     var what = q.querystring;
     
-    //indicate database and collection
-    var dbo = db.db("StockTicker");
-    var collection = dbo.collection('companies');
-    if(q.choice === "companyName"){
-      var result = collection.find({"Company": q.querystring});
-      console.log(result.Company);
-    } else {
-      collection.find({"Ticker": q.querystring});
-    }
+//     //indicate database and collection
+//     var dbo = db.db("StockTicker");
+//     var collection = dbo.collection('companies');
+//     if(q.choice === "companyName"){
+//       var result = collection.find({"Company": q.querystring});
+//       console.log(result.Company);
+//     } else {
+//       collection.find({"Ticker": q.querystring});
+//     }
   
-  //check whether it is company or ticker
-  //take the info and search for it
+//   //check whether it is company or ticker
+//   //take the info and search for it
   
-    res.end("The value is: " + what);
+//     res.end("The value is: " + what);
 
-    db.close();
-  });
+//     db.close();
+//   });
 }).listen(process.env.PORT||3000);
