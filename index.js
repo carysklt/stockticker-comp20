65b0000937b7f8ba92a40bb97d3db21d51f79825
carysklt <56458748+carysklt@users.noreply.github.com>
2020-12-11 21:54:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@
 // app.get('/', function(req,res){
 // //   MongoClient.connect(link, function(err, db) {
 // //     if(err) { throw err; }
-  
-// //     //take querystring from html form and parse it
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-// //     var q = url.parse(req.url, true).query;
-// //     var whichOne = q.choice;
-// //     var what = q.querystring;
     
 
 // //     if(q.choice === "companyName"){
@@ -25,16 +19,18 @@ const MongoClient = require('mongodb').MongoClient;
 const link = "mongodb+srv://carysklt:carysklt123@cluster0.nipyz.mongodb.net/?retryWrites=true&w=majority";
 
 http.createServer(function (req, res) {
+  MongoClient.connect(link, function(err,db){
   //indicate database and collection
-  var dbo = db.db('StockTicker');
-  var collection = dbo.collection('companies');
+//   var dbo = db.db('StockTicker');
+//   var collection = dbo.collection('companies');
   
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var txt = "Hello world";
+  //take query string from html form and parse it
   var q = url.parse(req.url, true).query;
   var whichOne = q.choice;
   var what = q.querystring;
   
+  });
   res.end("The value is: " + txt + what);
   //db.close();
 }).listen(process.env.PORT||8080);
